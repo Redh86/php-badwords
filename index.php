6c8fc514bd3bf95035ab8cb75496b6b643ab8fd8
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-05-04 18:44:22 +0200

    completo

--- index.php
+++ index.php
@@ -9,12 +9,13 @@
 <body>
 <!-- in php una variabile con una frase, stampare a video la frase e la lunghezza, stampare in output la frase sostituendo le parole con _get con *** -->
 <?php
-$badword = $_GET["gli"];
 $frase = "Se il coniglio ti ruba gli agli, togligli gli agli e tagliagli gli artigli";
-
-
+$badword = $_GET["badword"];
+$fraseFiltrata = str_replace($badword, "***", $frase);
 ?>
+
 <h1>La frase è di <?php echo strlen($frase)?> caratteri</h1>
 <p><?php echo ($frase)?></p>
+<p><?php echo ($fraseFiltrata)?></p>
 </body>
 </html>
